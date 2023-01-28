73202113b69ec3c0d18355388d2f423a0fb2f00d
Chris Cromer <chris@cromer.cl>
2023-01-27 23:51:05 -0300

    organize user routes

--- backend/routes/user.go
+++ backend/routes/user.go
@@ -9,9 +9,11 @@ import (
 
 func UserRoutes(router *httprouter.Router) {
 	router.POST("/login", controllers.Login)
-	router.GET("/users", middlewares.Authenticate(controllers.ListUsers))
-	router.POST("/user", middlewares.Authenticate(controllers.CreateUser))
-	router.GET("/user/:id", middlewares.Authenticate(controllers.GetUser))
-	router.PATCH("/user/:id", middlewares.Authenticate(controllers.UpdateUser))
 	router.GET("/auth", middlewares.Authenticate(controllers.AuthenticateUser))
+
+	router.GET("/users", middlewares.Authenticate(controllers.ListUsers))
+	router.GET("/user/:id", middlewares.Authenticate(controllers.GetUser))
+	router.POST("/user", middlewares.Authenticate(controllers.CreateUser))
+	router.PATCH("/user/:id", middlewares.Authenticate(controllers.UpdateUser))
+	router.DELETE("/user/:id", middlewares.Authenticate(controllers.DeleteUser))
 }

7c3e6e5809a2781752cb0d64a2b7e33970c5ced8
Chris Cromer <chris@cromer.cl>
2023-01-28 20:59:53 -0300

    extend the token lifetime

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -15,7 +15,7 @@ type JWTClaim struct {
 }
 
 func GenerateJWT(email string, username string) (tokenString string, err error) {
-	expirationTime := time.Now().Add(1 * time.Hour)
+	expirationTime := time.Now().Add(24 * time.Hour)
 	claims := &JWTClaim{
 		Email:    email,
 		Username: username,

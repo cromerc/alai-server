055dc9867e251934370825a8ee7e212952877746
Martin Araneda <martix.araneda@gmail.com>
2023-01-30 12:48:37 -0300

    basic chart menu finished

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AppLayout from '@/layout/AppLayout.vue';
 import HomeView from '../views/HomeView.vue';
+import Landing from '../views/Landing.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
+            component: Landing
+        },
+        {
+            path: '/home',
             component: AppLayout,
             children: [
                 {
-                    path: '/',
-                    name: 'home',
+                    path: '/home',
+                    name: 'Home',
                     component: HomeView
+                },
+                {
+                    path: '/charts',
+                    name: 'charts',
+                    component: () => import('@/views/menu/Chart.vue')
                 },
                 {
                     path: '/about',
@@ -20,6 +30,7 @@ const router = createRouter({
                     // route level code-splitting
                     // this generates a separate chunk (About.[hash].js) for this route
                     // which is lazy-loaded when the route is visited.
+
                     component: () => import('../views/AboutView.vue')
                 }
             ]

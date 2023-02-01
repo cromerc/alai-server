eb490c8b21f011b2194c860afdd44a287d051142
Chris Cromer <chris@cromer.cl>
2023-01-31 23:39:05 -0300

    add vite preview port

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -9,7 +9,7 @@ func Cors(handler http.Handler) http.Handler {
 		origin := request.Header.Get("Origin")
 
 		switch origin {
-		case "http://localhost:5173", "http://localhost", "https://alai.cromer.cl":
+		case "http://localhost:5173", "http://localhost:4173", "http://localhost", "https://alai.cromer.cl":
 			writer.Header().Set("Access-Control-Allow-Origin", origin)
 			writer.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH")
 			writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")

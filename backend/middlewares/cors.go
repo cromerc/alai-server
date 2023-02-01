523af1b0cb9e996d6bfebc63c4a62e18d93b8571
Chris Cromer <chris@cromer.cl>
2023-01-31 23:34:04 -0300

    add cors allow methods and headers

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -11,7 +11,8 @@ func Cors(handler http.Handler) http.Handler {
 		switch origin {
 		case "http://localhost:5173", "http://localhost", "https://alai.cromer.cl":
 			writer.Header().Set("Access-Control-Allow-Origin", origin)
-			writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			writer.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH")
+			writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 		}
 
 		handler.ServeHTTP(writer, request)

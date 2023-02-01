b1798085928aa8a375a5beb582c540b7a1560710
Chris Cromer <chris@cromer.cl>
2023-02-01 14:43:36 -0300

    add update and delete endpoints to game controller and router

--- backend/controllers/game.go
+++ backend/controllers/game.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"git.cromer.cl/Proyecto-Titulo/alai-server/backend/database"
 	"git.cromer.cl/Proyecto-Titulo/alai-server/backend/models"
@@ -88,5 +89,61 @@ func CreateGame(writer http.ResponseWriter, request *http.Request, params httpro
 	} else {
 		writer.WriteHeader(http.StatusOK)
 		return
+	}
+}
+
+func UpdateGame(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var game models.Game
+
+	decoder := json.NewDecoder(request.Body)
+
+	err := decoder.Decode(&game)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	game.ID, err = strconv.ParseUint(params.ByName("id"), 10, 64)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	result := gdb.Updates(&game)
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else if result.RowsAffected == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	} else {
+		writer.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func DeleteGame(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var game models.Game
+	var err error
+	game.ID, err = strconv.ParseUint(params.ByName("id"), 10, 64)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	result := gdb.Delete(&game)
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else if result.RowsAffected == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	} else {
+		writer.WriteHeader(http.StatusNoContent)
 	}
 }

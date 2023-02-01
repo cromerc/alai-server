fe8c80b1a462e578eeb90429b4dab6485c94717f
Chris Cromer <chris@cromer.cl>
2023-02-01 13:19:19 -0300

    add object state routes

---
+++ backend/controllers/object_state.go
@@ -0,0 +1,129 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"git.cromer.cl/Proyecto-Titulo/alai-server/backend/database"
+	"git.cromer.cl/Proyecto-Titulo/alai-server/backend/models"
+	"git.cromer.cl/Proyecto-Titulo/alai-server/backend/utils"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func ListObjectState(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var objectState []models.ObjectState
+
+	result := gdb.Model(&models.ObjectState{}).Order("ID asc").Find(&objectState)
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		json.NewEncoder(writer).Encode(objectState)
+	}
+}
+
+func GetObjectState(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var objectState models.ObjectState
+
+	result := gdb.Model(&models.ObjectState{}).Find(&objectState, params.ByName("id"))
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else if result.RowsAffected == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	} else {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		json.NewEncoder(writer).Encode(objectState)
+	}
+}
+
+func CreateObjectState(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var objectState models.ObjectState
+
+	decoder := json.NewDecoder(request.Body)
+
+	err := decoder.Decode(&objectState)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	result := gdb.Create(&objectState)
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else {
+		writer.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func UpdateObjectState(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var objectState models.ObjectState
+
+	decoder := json.NewDecoder(request.Body)
+
+	err := decoder.Decode(&objectState)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	objectState.ID, err = strconv.ParseUint(params.ByName("id"), 10, 64)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	result := gdb.Updates(&objectState)
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else if result.RowsAffected == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	} else {
+		writer.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func DeleteObjectState(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	gdb := database.Connect()
+	defer database.Close(gdb)
+
+	var objectState models.ObjectState
+	var err error
+	objectState.ID, err = strconv.ParseUint(params.ByName("id"), 10, 64)
+	if err != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	result := gdb.Delete(&objectState)
+	if result.Error != nil {
+		utils.JSONErrorOutput(writer, http.StatusBadRequest, result.Error.Error())
+		return
+	} else if result.RowsAffected == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	} else {
+		writer.WriteHeader(http.StatusNoContent)
+	}
+}

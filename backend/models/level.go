0f1bff66dcc123fc3296be006906493c5dbd959b
Chris Cromer <chris@cromer.cl>
2023-02-07 23:31:06 -0300

    fix incorrect json variable name

--- backend/models/level.go
+++ backend/models/level.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Level struct {
 	gorm.Model
 	ID   uint64 `json:"ID" gorm:"primaryKey"`
-	Name string `json:"rut" gorm:"unique;not null"`
+	Name string `json:"name" gorm:"unique;not null"`
 }

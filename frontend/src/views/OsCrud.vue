3b03b4ba31381d03467acd42ad6c5269a4588c08
Martin Araneda <martix.araneda@gmail.com>
2023-02-14 18:23:22 -0300

    minor fixes to Users,OS crud

--- frontend/src/views/OsCrud.vue
+++ frontend/src/views/OsCrud.vue
@@ -6,13 +6,13 @@ import axios from 'axios';
 import auth from '../utils/Auth';
 
 const toast = useToast();
-const products = ref(null);
-const productDialog = ref(false);
-const productDialogChange = ref(false);
-const deleteProductDialog = ref(false);
-const deleteProductsDialog = ref(false);
+const users = ref(null);
+const userDialog = ref(false);
+const userDialogChange = ref(false);
+const deleteUserDialog = ref(false);
+const deleteUsersDialog = ref(false);
 const os = ref({});
-const selectedProducts = ref(null);
+const selectedUsers = ref(null);
 const dt = ref(null);
 const filters = ref({});
 const submitted = ref(false);
@@ -20,11 +20,11 @@ const checkAuth = () => {
     auth.checkToken(true);
 };
 
-async function onShowClick() {
+async function showTable() {
     try {
         const response = await axios.get(`http://localhost:3001/os`);
         console.log(response.data);
-        products.value = response.data;
+        users.value = response.data;
     }
     catch (error) {
         console.error(error);
@@ -45,7 +45,8 @@ async function onCreateClick() {
 
         if (response.status === 204) {
             toast.add({ severity: 'success', summary: 'Successful', detail: 'OS Created', life: 3000 });
-            productDialog.value = false;
+            userDialog.value = false;
+            showTable();
         }
         else {
             console.error(response);
@@ -62,28 +63,29 @@ onBeforeMount(() => {
 });
 onMounted(() => {
     checkAuth();
+    showTable();
 });
 
 const openNew = () => {
     os.value = {};
     submitted.value = false;
-    productDialog.value = true;
+    userDialog.value = true;
 };
 
 const hideDialog = () => {
-    productDialog.value = false;
+    userDialog.value = false;
     submitted.value = false;
 };
 
 const hideDialogChange = () => {
-    productDialogChange.value = false;
+    userDialogChange.value = false;
     submitted.value = false;
 };
 
-const editProduct = (editProduct) => {
-    os.value = { ...editProduct };
+const editUser = (editUser) => {
+    os.value = { ...editUser };
     console.log(os.value.ID);
-    productDialogChange.value = true;
+    userDialogChange.value = true;
 };
 async function onChangeOS() {
     var changeOS =
@@ -96,7 +98,8 @@ async function onChangeOS() {
 
         if (response.status === 204) {
             toast.add({ severity: 'success', summary: 'Successful', detail: 'OS Modified', life: 3000 });
-            productDialogChange.value = false;
+            userDialogChange.value = false;
+            showTable();
         }
         else {
             console.error(response);
@@ -107,33 +110,34 @@ async function onChangeOS() {
     }
 }
 
-const confirmDeleteProduct = (editProduct) => {
-    os.value = editProduct;
-    deleteProductDialog.value = true;
+const confirmDeleteUser = (editUser) => {
+    os.value = editUser;
+    deleteUserDialog.value = true;
 };
 
-async function deleteProduct() {
+async function deleteUser() {
     try {
         const response = await axios.delete(`http://localhost:3001/os/` + os.value.ID, auth.getTokenHeader());
         if (response.status !== 204) {
             console.error(response);
         }
+        showTable();
     }
     catch (error) {
         console.error(error);
     }
-    products.value = products.value.filter((val) => val.ID !== os.value.ID);
-    deleteProductDialog.value = false;
+    users.value = users.value.filter((val) => val.ID !== os.value.ID);
+    deleteUserDialog.value = false;
     os.value = {};
     toast.add({ severity: 'success', summary: 'Successful', detail: 'OS Deleted', life: 3000 });
 };
 
 
 const confirmDeleteSelected = () => {
-    deleteProductsDialog.value = true;
+    deleteUsersDialog.value = true;
 };
-const deleteSelectedProducts = () => {
-    selectedProducts.value.forEach(element => {
+const deleteSelectedUsers = () => {
+    selectedUsers.value.forEach(element => {
         try {
             const response = axios.delete(`http://localhost:3001/os/` + element.ID, auth.getTokenHeader());
             if (response.status !== 204) {
@@ -145,12 +149,12 @@ const deleteSelectedProducts = () => {
         }
 
     });
-    console.log(selectedProducts.value);
+    console.log(selectedUsers.value);
 
-    products.value = products.value.filter((val) => !selectedProducts.value.includes(val));
-    deleteProductsDialog.value = false;
-    selectedProducts.value = null;
-    toast.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+    users.value = users.value.filter((val) => !selectedUsers.value.includes(val));
+    deleteUsersDialog.value = false;
+    selectedUsers.value = null;
+    toast.add({ severity: 'success', summary: 'Successful', detail: 'Users Deleted', life: 3000 });
 };
 
 const initFilters = () => {
@@ -169,25 +173,23 @@ const initFilters = () => {
                     <template v-slot:start>
                         <div class="my-2">
                             <Button label="New" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" />
-                            <Button label="Show" icon="pi pi-eye" class="p-button-success mr-2" @click="onShowClick" />
                             <Button label="Delete" icon="pi pi-trash" class="p-button-danger"
-                                @click="confirmDeleteSelected"
-                                :disabled="!selectedProducts || !selectedProducts.length" />
+                                @click="confirmDeleteSelected" :disabled="!selectedUsers || !selectedUsers.length" />
                         </div>
                     </template>
 
 
                 </Toolbar>
 
-                <DataTable ref="dt" :value="products" v-model:selection="selectedProducts" dataKey="ID"
-                    :paginator="true" :rows="10" :filters="filters"
+                <DataTable ref="dt" :value="users" v-model:selection="selectedUsers" dataKey="ID" :paginator="true"
+                    :rows="10" :filters="filters"
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     :rowsPerPageOptions="[5, 10, 25]"
-                    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+                    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} users"
                     responsiveLayout="scroll">
                     <template #header>
                         <div class="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
-                            <h5 class="m-0">Manage Products</h5>
+                            <h5 class="m-0">Manage Users</h5>
                             <span class="block mt-2 md:mt-0 p-input-icon-left">
                                 <i class="pi pi-search" />
                                 <InputText v-model="filters['global'].value" placeholder="Search..." />
@@ -205,14 +207,14 @@ const initFilters = () => {
                     <Column headerStyle="min-width:10rem;">
                         <template #body="slotProps">
                             <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2"
-                                @click="editProduct(slotProps.data)" />
+                                @click="editUser(slotProps.data)" />
                             <Button icon="pi pi-trash" class="p-button-rounded p-button-warning mt-2"
-                                @click="confirmDeleteProduct(slotProps.data)" />
+                                @click="confirmDeleteUser(slotProps.data)" />
                         </template>
                     </Column>
                 </DataTable>
 
-                <Dialog v-model:visible="productDialog" :style="{ width: '450px' }" header="New OS" :modal="true"
+                <Dialog v-model:visible="userDialog" :style="{ width: '450px' }" header="New OS" :modal="true"
                     class="p-fluid">
                     <div class="field">
                         <label for="name">Name</label>
@@ -225,7 +227,7 @@ const initFilters = () => {
                         <Button label="Save" icon="pi pi-check" class="p-button-text" @click="onCreateClick" />
                     </template>
                 </Dialog>
-                <Dialog v-model:visible="productDialogChange" :style="{ width: '450px' }" header="New OS" :modal="true"
+                <Dialog v-model:visible="userDialogChange" :style="{ width: '450px' }" header="New OS" :modal="true"
                     class="p-fluid">
                     <div class="field">
                         <label for="name">Name</label>
@@ -239,29 +241,26 @@ const initFilters = () => {
                     </template>
                 </Dialog>
 
-                <Dialog v-model:visible="deleteProductDialog" :style="{ width: '450px' }" header="Confirm"
-                    :modal="true">
+                <Dialog v-model:visible="deleteUserDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
                     <div class="flex align-items-center justify-content-center">
                         <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
                         <span v-if="os">Are you sure you want to delete <b>{{ os.name }}</b>?</span>
                     </div>
                     <template #footer>
-                        <Button label="No" icon="pi pi-times" class="p-button-text"
-                            @click="deleteProductDialog = false" />
-                        <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteProduct" />
+                        <Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteUserDialog = false" />
+                        <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteUser" />
                     </template>
                 </Dialog>
 
-                <Dialog v-model:visible="deleteProductsDialog" :style="{ width: '450px' }" header="Confirm"
-                    :modal="true">
+                <Dialog v-model:visible="deleteUsersDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
                     <div class="flex align-items-center justify-content-center">
                         <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-                        <span v-if="os">Are you sure you want to delete the selected products?</span>
+                        <span v-if="os">Are you sure you want to delete the selected users?</span>
                     </div>
                     <template #footer>
                         <Button label="No" icon="pi pi-times" class="p-button-text"
-                            @click="deleteProductsDialog = false" />
-                        <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteSelectedProducts" />
+                            @click="deleteUsersDialog = false" />
+                        <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteSelectedUsers" />
                     </template>
                 </Dialog>
             </div>
